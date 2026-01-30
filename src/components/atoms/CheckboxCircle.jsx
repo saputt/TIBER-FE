beb1fdb624a9f6d2c3d422656081859dace0e99d
saputt29 <sauki666indo@gmail.com>
2026-01-30 15:55:55 +0700

    feat : add  CheckBoxCircle atoms

---
+++ src/components/atoms/CheckboxCircle.jsx
@@ -0,0 +1,12 @@
+import { CircleCheck } from "lucide-react";
+import React from "react";
+
+const CheckboxCircle = () => {
+  return (
+    <div>
+      <CircleCheck size={30} />
+    </div>
+  );
+};
+
+export default CheckboxCircle;
--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import TimePickerSlider from "../Auth/components/TimePickerSlider";
+import CheckboxCircle from "../../components/atoms/CheckboxCircle";
 
 const LandingPage = () => {
   return <div></div>;
