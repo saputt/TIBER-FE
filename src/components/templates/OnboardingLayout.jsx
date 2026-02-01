00eb141166726eabe9bd7d08c3882e467993c177
saputt29 <sauki666indo@gmail.com>
2026-02-01 08:29:34 +0700

    feat : add onboardinglayout

--- src/components/templates/OnboardingLayout.jsx
+++ src/components/templates/OnboardingLayout.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import Navbar from "../organism/Navbar";
+import Button from "../atoms/Button";
+import ButtonTest from "../atoms/ButtonTest";
+import { ArrowRight } from "lucide-react";
 
-const OnboardingLayout = () => {
+const OnboardingLayout = ({ step, children }) => {
   return (
-    <div>
-      <Navbar variant="setup" />
+    <div className="bg-gray-100">
+      <Navbar variant="setup" step={step} />
+
+      <main>
+        <section>{children}</section>
+
+        <footer className="bg-white sticky bottom-0 px-6 py-3 border-t-1 border-gray-200">
+          <ButtonTest
+            variant="primary"
+            size="full"
+            className="flex items-center gap-3"
+          >
+            <p className="font-bold text-h4">Lanjut</p>
+            <ArrowRight className="w-15 text-white" />
+          </ButtonTest>
+        </footer>
+      </main>
     </div>
   );
 };
--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import ButtonTest from "../../components/atoms/ButtonTest";
 import Navbar from "../../components/organism/Navbar";
+import OnboardingLayout from "../../components/templates/OnboardingLayout";
 
 const LandingPage = () => {
   return (
-    <div>
-      <Navbar variant="landing" />
-    </div>
+    <OnboardingLayout>
+      <p>tes</p>
+    </OnboardingLayout>
   );
 };
 
