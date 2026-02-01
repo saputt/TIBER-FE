94b6e27226d5253340abec9c8890462350ab7112
saputt29 <sauki666indo@gmail.com>
2026-02-01 07:52:42 +0700

    feat : refactor structure page

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,66 +1,66 @@
 import { createBrowserRouter } from "react-router-dom";
-import LoginPage from "../pages/Auth/login";
-import RegisterPage from "../pages/Auth/register";
-import OnboardingPage from "../pages/Auth/onboarding";
+import OnboardingPage from "../pages/Register/onboarding";
 import LandingPage from "../pages/Landing";
 import ActivityPage from "../pages/Activity";
 import ProgressPage from "../pages/Progress";
 import ProfilePage from "../pages/Profile";
 import DashboardPage from "../pages/Dashboard";
 import NotFoundPage from "../pages/404";
+import RegisterPage from "../pages/Register/Register";
+import LoginPage from "../pages/Login/Login";
 
 const router = createBrowserRouter([
-    //auth page
-    {
-        path : '/login',
-        element : <LoginPage/>
-    },
-    {
-        path : '/register',
-        element : <RegisterPage/>
-    },
+  //auth page
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
 
-    //onboarding (personalization)
-    {
-        path : '/onboarding',
-        element : <OnboardingPage/>
-    },
+  //onboarding (personalization)
+  {
+    path: "/onboarding",
+    element: <OnboardingPage />,
+  },
 
-    //landing page
-    {
-        path : '/',
-        element : <LandingPage/>
-    },
+  //landing page
+  {
+    path: "/",
+    element: <LandingPage />,
+  },
 
-    //activity page
-    {
-        path : '/activity',
-        element : <ActivityPage/>
-    },
+  //activity page
+  {
+    path: "/activity",
+    element: <ActivityPage />,
+  },
 
-    //progress page
-    {
-        path : '/progress',
-        element : <ProgressPage/>
-    },
+  //progress page
+  {
+    path: "/progress",
+    element: <ProgressPage />,
+  },
 
-    //profile page
-    {
-        path : '/profile',
-        element : <ProfilePage/>
-    },
+  //profile page
+  {
+    path: "/profile",
+    element: <ProfilePage />,
+  },
 
-    //home page (dashboard)
-    {
-        path : '/dashboard',
-        element : <DashboardPage/>
-    },
+  //home page (dashboard)
+  {
+    path: "/dashboard",
+    element: <DashboardPage />,
+  },
 
-    //404 page
-    {
-        path : '*',
-        element : <NotFoundPage/>
-    }
-])
+  //404 page
+  {
+    path: "*",
+    element: <NotFoundPage />,
+  },
+]);
 
-export default router
+export default router;
