d1f5c80517a8285225ae527a9e69cb364204c5fc
saputt29 <sauki666indo@gmail.com>
2026-02-04 13:50:36 +0700

    feat : add classname props to notch atom

--- src/components/atoms/Notch.jsx
+++ src/components/atoms/Notch.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Notch = ({ variant, children }) => {
+const Notch = ({ variant, children, className }) => {
   const styles = {
     left: "rounded-br-lg top-0 left-0",
     right: "rounded-bl-lg top-0 right-0",
@@ -9,7 +9,9 @@ const Notch = ({ variant, children }) => {
   const defaultStyle =
     "bg-primary text-white text-h6 font-semibold px-5 px-1 absolute text-center";
   return (
-    <div className={` ${styles[variant]} ${defaultStyle}`}>{children}</div>
+    <div className={`${className} ${styles[variant]} ${defaultStyle}`}>
+      {children}
+    </div>
   );
 };
 
