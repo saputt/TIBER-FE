c21fa9a9b41ee55a91807054ae2a90a3d076e6bd
saputt29 <sauki666indo@gmail.com>
2026-02-05 04:43:06 +0700

    feat : edit selection button

--- src/components/atoms/SelectionButton.jsx
+++ src/components/atoms/SelectionButton.jsx
@@ -1,31 +1,44 @@
-import { CircleCheck } from "lucide-react";
+import { CircleCheck, CircleCheckBig } from "lucide-react";
 import React from "react";
 
 const SelectionButton = ({
-  children,
   isSelect,
   selectionName,
   className,
   onClick,
+  isBoarding,
 }) => {
   const defaultStyle = " text-center cursor-pointer";
 
   let style;
 
-  if (isSelect) {
+  if (isSelect && isBoarding) {
     style = "bg-primary/30 border-primary flex items-center px-5";
+  } else if (isSelect) {
+    style = "bg-primary px-5 text-white";
   } else if (!isSelect) {
     style = "bg-white border-gray-400 border-1 px-5";
   }
   return (
     <>
+      {isSelect && isBoarding && (
+        <button
+          className={`${style} ${defaultStyle} ${className} flex justify-center items-center relative`}
+          onClick={onClick}
+          value={selectionName}
+        >
+          <p>{selectionName}</p>
+          <CircleCheckBig size={25} className="text-primary absolute right-3" />
+        </button>
+      )}
+
       {isSelect && (
         <button
           className={`${style} ${defaultStyle} ${className}`}
           onClick={onClick}
           value={selectionName}
         >
-          {children}
+          <p>{selectionName}</p>
         </button>
       )}
 
@@ -35,7 +48,7 @@ const SelectionButton = ({
           onClick={onClick}
           value={selectionName}
         >
-          {children}
+          {selectionName}
         </button>
       )}
     </>
