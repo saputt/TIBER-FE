0a1c553bad58c3afcdda7f2d7589f64dca2a20c9
saputt <sauki666indo@gmail.com>
2026-02-05 14:34:57 +0700

    feat : add page activity

--- src/pages/Activity/index.jsx
+++ src/pages/Activity/index.jsx
@@ -1,9 +1,19 @@
 import React, { useState } from "react";
+import SelectionButton from "../../components/atoms/SelectionButton";
+import CalenderWeek from "./components/CalenderWeek";
+import StreakCard from "./components/StreakCard";
+import History from "./components/History";
 
 const ActivityPage = () => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-3">
       <h3 className="font-bold text-h3">Aktivitas Saya</h3>
+      <button className="text-h6 bg-primary px-5 py-1 rounded-xl w-fit text-white">
+        Minum Obat
+      </button>
+      <CalenderWeek />
+      <StreakCard />
+      <History />
     </div>
   );
 };
