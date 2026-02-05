77444f2eb9a8dfa0c6b731fe529b7b30da84c380
saputt <sauki666indo@gmail.com>
2026-02-05 14:43:34 +0700

    feat : add state to log card

--- src/pages/Dashboard/components/CardLog.jsx
+++ src/pages/Dashboard/components/CardLog.jsx
@@ -1,42 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../../../components/atoms/Card";
 import Badge from "../../../components/atoms/Badge";
-import { Pill } from "lucide-react";
+import { CircleCheckBig, Pill } from "lucide-react";
 import ButtonTest from "../../../components/atoms/ButtonTest";
 
 const CardLog = ({ isTaken }) => {
+  const [taken, setTaken] = useState(false);
   return (
     <>
-      {isTaken && (
+      {taken && (
         <Card
           boxShadowActive={true}
           size="full"
-          variant="white"
-          className="flex flex-col gap-3 py-4"
+          className="flex flex-col gap-3 py-4 bg-secondary"
         >
-          <Badge variant="confirmGray" size="md" className="text-h7">
+          <Badge
+            variant="confirmPrimary"
+            size="md"
+            className="text-h7 text-black"
+          >
             Hari ini belum dicatat
           </Badge>
 
-          <Pill size={30} className=" text-primary" />
+          <CircleCheckBig size={35} className=" text-primary" />
 
-          <h4 className="text-h4 text-wrap">Konfirmasi Minum Obat Hari Ini</h4>
+          <h4 className="text-h4 text-wrap w-50">
+            Terima kasih sudah menjaga diri hari ini.
+          </h4>
 
           <p className="text-h6 text-wrap w-[60%]">
-            Catat rutinitas harianmu dengan satu ketukan
+            Bagus! Catatanmu sudah tersimpan aman.
           </p>
 
           <ButtonTest
-            variant="primary"
+            variant="secondary"
             size="full"
             className="font-bold text-h6"
+            onClick={() => setTaken(!taken)}
           >
-            Konfirmasi
+            Sampai jumpa di jadwal berikutnya
           </ButtonTest>
         </Card>
       )}
 
-      {!isTaken && (
+      {!taken && (
         <Card
           boxShadowActive={true}
           size="full"
@@ -59,6 +66,7 @@ const CardLog = ({ isTaken }) => {
             variant="primary"
             size="full"
             className="font-bold text-h6"
+            onClick={() => setTaken(!taken)}
           >
             Konfirmasi
           </ButtonTest>
